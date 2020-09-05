20eb9103be2d0901917c1c6781356b7e4220f40c
Nunya Business <nunya@business.com>
2020-09-05 00:34:40 -0700

    added powershell eventlogs

--- Eventlog_QuickWins/Program.cs
+++ Eventlog_QuickWins/Program.cs
@@ -47,15 +47,18 @@ namespace Eventlog_QuickWins
                                              x.MachineName,
                                              x.Site,
                                              x.Source,
-                                             x.Message
+                                             x.Message,
+                                             x.TimeGenerated,
                                          }).ToList();
 
                 foreach (var entry in entries)
                 {
                     // check if its a 4624 type 10 or 7
-                    if(entry.Message.Contains("Logon Type\t10") || !entry.Message.Contains("Logon Type\t7"))
+                    if(entry.Message.Contains("Logon Type\t10") || !entry.Message.Contains("Logon Type\t5"))
                     {
                         Console.WriteLine("--------------------------------");
+                        Console.WriteLine(entry.TimeGenerated.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                        Console.WriteLine("EventID: 4624");
                         string[] wordsToMatch = { "Logon Type", "Workstation Name", "Source Network Address", "Network Account Name", "Account Name", "Account Domain" };
 
                         using (StringReader reader = new StringReader(entry.Message))
@@ -76,8 +79,106 @@ namespace Eventlog_QuickWins
 
                 }
             }
+
+            // Powershell
+
+           
+            using (StreamWriter writer = new StreamWriter(csPath + HostName + "_EventlogPowershell.txt"))
+            {
+                Console.SetOut(writer);
+                
+                // Powershell operational
+                using (var reader = new EventLogReader(@"C:\Windows\System32\winevt\Logs\Microsoft-Windows-PowerShell%4Operational.evtx", PathType.FilePath))
+                {
+                    EventRecord record;
+                    while ((record = reader.ReadEvent()) != null)
+                    {
+                        using (record)
+                        {
+                            if (record.Id == 4104)
+                            {
+                                Console.WriteLine("--------------------------------");
+                                Console.WriteLine(DateTime.Parse(record.TimeCreated.ToString()).ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                                Console.WriteLine("EventID: " + record.Id);
+                                Console.WriteLine(record.FormatDescription());
+                                Console.WriteLine("--------------------------------");
+                            }
+                            
+
+                        }
+                    }
+                }
+                
+
+                // Microsoft-Windows-PowerShell
+                EventLog log = new EventLog("Windows PowerShell");
+                var entries = log.Entries.Cast<EventLogEntry>()
+                                         .Where(x => x.InstanceId == 600)
+                                         .Select(x => new
+                                         {
+                                             x.MachineName,
+                                             x.Site,
+                                             x.Source,
+                                             x.Message,
+                                             x.TimeGenerated,
+                                         }).ToList();
+                
+                foreach (var entry in entries)
+                {
+
+                    
+                    bool shouldPrint = false;
+                    using (StringReader reader = new StringReader(entry.Message))
+                    {
+                        string line = string.Empty;
+                        do
+                        {
+                            line = reader.ReadLine();
+                            if (line != null)
+                            {
+                                if (line.Contains("HostApplication") && !line.EndsWith("powershell.exe") || (line.Contains("ScriptName=") && !line.EndsWith("ScriptName=")) || (line.Contains("CommandLine=") && !line.EndsWith("CommandLine="))) // if there's something non-default
+                                {
+                                    shouldPrint = true;
+                                } 
+
+                            }
+
+                        } while (line != null);
+                    }
+
+                    if (shouldPrint)
+                    {
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine(entry.TimeGenerated.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                        Console.WriteLine("EventID: 600");
+
+                        using (StringReader reader = new StringReader(entry.Message))
+                        {
+                            string line = string.Empty;
+                            do
+                            {
+                                line = reader.ReadLine();
+                                if (line != null)
+                                {
+                                    if (line.Contains("HostApplication") && !line.EndsWith("powershell.exe") || (line.Contains("ScriptName=") && !line.EndsWith("ScriptName=")) || (line.Contains("CommandLine=") && !line.EndsWith("CommandLine="))) // if there's something non-default
+                                    {
+                                        Console.WriteLine(line.Replace("\t", ""));
+                                    }
+                                }
+
+                            } while (line != null);
+                        }
+
+                        Console.WriteLine("--------------------------------");
+                    }
+
+                    
+
+                }
+            }
+
             
-            //Console.ReadLine();
+
         }
     }
 }
