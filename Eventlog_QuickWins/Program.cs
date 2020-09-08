9e13183dc447d55656d60748ac1a9ae49697cd6d
Nunya Business <nunya@business.com>
2020-09-07 18:00:20 -0700

    added terminalservices-localsessionmanager

--- Eventlog_QuickWins/Program.cs
+++ Eventlog_QuickWins/Program.cs
@@ -10,13 +10,13 @@ using System.Threading.Tasks;
 
 namespace Eventlog_QuickWins
 {
-    class Program
+    public class Program
     {
 
         public static void printRelevantInfo(String inputString, String[] items)
         {
             bool found = false;
-            foreach(string item in items)
+            foreach (string item in items)
             {
                 if (inputString.Contains(item))
                 {
@@ -30,7 +30,7 @@ namespace Eventlog_QuickWins
             }
         }
 
-        static void Main(string[] args)
+        public static void Main()
         {
 
             string HostName = Dns.GetHostName();
@@ -39,6 +39,8 @@ namespace Eventlog_QuickWins
             using (StreamWriter writer = new StreamWriter(csPath + HostName + "_EventlogRDP.txt"))
             {
                 Console.SetOut(writer);
+
+                // First check the security eventlog for RDPs
                 EventLog log = new EventLog("Security");
                 var entries = log.Entries.Cast<EventLogEntry>()
                                          .Where(x => x.InstanceId == 4624)
@@ -54,7 +56,7 @@ namespace Eventlog_QuickWins
                 foreach (var entry in entries)
                 {
                     // check if its a 4624 type 10 or 7
-                    if(entry.Message.Contains("Logon Type\t10") || !entry.Message.Contains("Logon Type\t5"))
+                    if (entry.Message.Contains("Logon Type\t10") || !entry.Message.Contains("Logon Type\t7"))
                     {
                         Console.WriteLine("--------------------------------");
                         Console.WriteLine(entry.TimeGenerated.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
@@ -78,15 +80,38 @@ namespace Eventlog_QuickWins
                     }
 
                 }
+
+                // Then check Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx for logons
+                using (var reader = new EventLogReader(@"C:\Windows\System32\winevt\Logs\Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx", PathType.FilePath))
+                {
+                    EventRecord record;
+                    while ((record = reader.ReadEvent()) != null)
+                    {
+                        using (record)
+                        {
+                            if (record.Id == 21 || record.Id == 23 || record.Id == 24)
+                            {
+                                Console.WriteLine("--------------------------------");
+                                Console.WriteLine(DateTime.Parse(record.TimeCreated.ToString()).ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                                Console.WriteLine("EventID: " + record.Id);
+                                Console.WriteLine(record.FormatDescription());
+                                Console.WriteLine("--------------------------------");
+                            }
+
+
+                        }
+                    }
+                }
+
             }
 
             // Powershell
 
-           
+
             using (StreamWriter writer = new StreamWriter(csPath + HostName + "_EventlogPowershell.txt"))
             {
                 Console.SetOut(writer);
-                
+
                 // Powershell operational
                 using (var reader = new EventLogReader(@"C:\Windows\System32\winevt\Logs\Microsoft-Windows-PowerShell%4Operational.evtx", PathType.FilePath))
                 {
@@ -103,17 +128,17 @@ namespace Eventlog_QuickWins
                                 Console.WriteLine(record.FormatDescription());
                                 Console.WriteLine("--------------------------------");
                             }
-                            
+
 
                         }
                     }
                 }
-                
+
 
                 // Microsoft-Windows-PowerShell
                 EventLog log = new EventLog("Windows PowerShell");
                 var entries = log.Entries.Cast<EventLogEntry>()
-                                         .Where(x => x.InstanceId == 600)
+                                         //.Where(x => x.InstanceId == 600)
                                          .Select(x => new
                                          {
                                              x.MachineName,
@@ -121,12 +146,13 @@ namespace Eventlog_QuickWins
                                              x.Source,
                                              x.Message,
                                              x.TimeGenerated,
+                                             x.InstanceId,
                                          }).ToList();
-                
+
                 foreach (var entry in entries)
                 {
 
-                    
+
                     bool shouldPrint = false;
                     using (StringReader reader = new StringReader(entry.Message))
                     {
@@ -139,7 +165,7 @@ namespace Eventlog_QuickWins
                                 if (line.Contains("HostApplication") && !line.EndsWith("powershell.exe") || (line.Contains("ScriptName=") && !line.EndsWith("ScriptName=")) || (line.Contains("CommandLine=") && !line.EndsWith("CommandLine="))) // if there's something non-default
                                 {
                                     shouldPrint = true;
-                                } 
+                                }
 
                             }
 
@@ -150,7 +176,7 @@ namespace Eventlog_QuickWins
                     {
                         Console.WriteLine("--------------------------------");
                         Console.WriteLine(entry.TimeGenerated.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
-                        Console.WriteLine("EventID: 600");
+                        Console.WriteLine("EventID: " + entry.InstanceId);
 
                         using (StringReader reader = new StringReader(entry.Message))
                         {
@@ -172,12 +198,12 @@ namespace Eventlog_QuickWins
                         Console.WriteLine("--------------------------------");
                     }
 
-                    
+
 
                 }
             }
 
-            
+
 
         }
     }
